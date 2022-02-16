3a835ea7c61ce10f9687ad85db8f44ab1b19df45
Barış Erbay <99364084+avbrsrby@users.noreply.github.com>
2022-02-16 18:19:29 +0300

    Add files via upload

--- Daire Alan ve Çevresi Hesaplama.py
+++ Daire Alan ve Çevresi Hesaplama.py
@@ -3,3 +3,5 @@ pi = 3.14
 daire_alani = pi*(daire_yaricap**2)
 daire_cevresi = 2 * pi * daire_yaricap
 print('Daire Alanınız :' + str((daire_alani)) , ('  ---   Daire Çevreniz : ' + str(round(daire_cevresi,2))))
+print(f'Daire Alanınız : {daire_alani} ---  Daire Çevreniz : {round(daire_cevresi,2)}' ) 
+
